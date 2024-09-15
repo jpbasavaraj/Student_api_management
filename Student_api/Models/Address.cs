f364793f03e15d0d8546320f12cedb44f54973d7
ADMIN <jpbasavaraj118@gmail.com>
2024-09-15 10:53:06 +0530

    [student Model created]

---
+++ Student_api/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿namespace Student_api.Models
+{
+    public class Address
+    {
+
+        public string AdressLineOne { get; set; }
+        public string AdressLineTwo { get; set; }
+        public string City { get; set; }
+        public string pincode { get; set; }
+        public string Region { get; set; }
+        public string Country   { get; set; }
+    }
+}
---
+++ Student_api/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Student_api.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string MotherName { get; set; }
+        public string FatherName { get; set; }
+        public string StudentEmail { get; set; }
+        public string StudentPhone { get; set; }
+        public Address StudentAdress {get;set;}
+
+    }
+}
